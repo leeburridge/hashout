19807747ae80ad1f27027a90e06faaedb0d14a33
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2018-04-18 10:11:35 +0100

    First commit

---
+++ hash.ps1
@@ -0,0 +1,13 @@
+function Hash($textToHash)
+{
+  $hasher = new-object System.Security.Cryptography.SHA256Managed
+  $toHash = [System.Text.Encoding]::UTF8.GetBytes($textToHash)
+  $hashByteArray = $hasher.ComputeHash($toHash)
+  foreach($byte in $hashByteArray) 
+    {
+      $res += $byte.ToString()
+    }
+  return $res;
+}
+
+Hash("Hello")
